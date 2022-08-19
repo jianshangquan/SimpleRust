342629f305fe3c4831d33d372c6937f514d68f5a
jianshangquan <jianshangquan3@gmail.com>
2022-08-19 16:28:43 +0630

    ownership and borrowing rule

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,32 @@
-mod structs;
-mod enums;
+use std::fmt::Display;
+
+
+
+struct People{
+    age: u128
+}
+
+impl Display for People {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "age {}", self.age)
+    }
+}
+
 fn main() {
     println!("Hello, world!");
-    let student = structs::init();
-    student.go();
+    // let student = structs::init();
+    // student.go();
 
-    let lang = enums::ComputerLanguage::Cpp;
-    enums::language(lang);
+    // let lang = enums::ComputerLanguage::Cpp;
+    // enums::language(lang);
+
+    let mut outsideValue: String = String::from("123456");
+    passValueFromOutside(&mut outsideValue);
+    println!("{}", outsideValue);
+}
+
+
+fn passValueFromOutside(value: &mut String){
+    *value = String::from("new string");
+    println!("{}",value);
 }
